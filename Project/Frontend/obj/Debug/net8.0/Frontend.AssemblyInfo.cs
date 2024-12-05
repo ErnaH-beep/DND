39f373dc085b913188dbfaee9bf8aab0db3896ed
MathiasLindekildeRasmussen <mathias.lindekilde@gmail.com>
2024-12-05 13:10:19 +0100

    changed nav button

--- Project/Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Project/Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ac049e743b0f70ed954bd6bd69605259df44b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98ca6ade92ccf1b8fd3388d2dbeca0a3fa167a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
